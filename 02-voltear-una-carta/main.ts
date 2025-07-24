6c97136481f55c61e532ca334513cce042267ef2
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-07-24 13:12:54 +0200

    solucionando problema del div vacío

--- 02-voltear-una-carta/main.ts
+++ 02-voltear-una-carta/main.ts
@@ -4,14 +4,13 @@ const carta = document.getElementById("carta") as HTMLImageElement;
 
 if (carta) {
   carta.addEventListener("click", () => {
-    carta.classList.remove("flip-vertical-right"); // por si ya la tenía
+    carta.classList.remove("flip-vertical-right", "carta");
     carta.classList.add("flip-vertical-left");
     carta.src = "/img/1.png"
 
     setTimeout(() => {
       carta.classList.remove("flip-vertical-left");
-      carta.classList.add("flip-vertical-right");
-      carta.src = "";
+      carta.classList.add("flip-vertical-right", "carta-vacia");
     }, 1500);
   });
 }

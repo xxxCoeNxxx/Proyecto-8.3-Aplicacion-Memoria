22e4f13419cb754ab3bb53e79ed94dbebc0feb0c
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-07-24 12:32:18 +0200

    problema con src vacío

--- 02-voltear-una-carta/main.ts
+++ 02-voltear-una-carta/main.ts
@@ -4,7 +4,14 @@ const carta = document.getElementById("carta") as HTMLImageElement;
 
 if (carta) {
   carta.addEventListener("click", () => {
-    
-    carta.src = "/img/2.png";
-  })
+    carta.classList.remove("flip-vertical-right"); // por si ya la tenía
+    carta.classList.add("flip-vertical-left");
+    carta.src = "/img/1.png"
+
+    setTimeout(() => {
+      carta.classList.remove("flip-vertical-left");
+      carta.classList.add("flip-vertical-right");
+      carta.src = "";
+    }, 1500);
+  });
 }

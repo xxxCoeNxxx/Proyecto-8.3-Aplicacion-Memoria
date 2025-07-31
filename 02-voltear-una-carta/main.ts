f48d65042ccd5c2cda07a653ff1ea5fb213565e7
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-07-31 12:57:49 +0200

    voltear carta resuelto

--- 02-voltear-una-carta/main.ts
+++ 02-voltear-una-carta/main.ts
@@ -1,16 +1,17 @@
 import "./style.css";
 
-const carta = document.getElementById("carta") as HTMLImageElement;
+const carta = document.getElementById("carta") as HTMLDivElement;
 
 if (carta) {
   carta.addEventListener("click", () => {
-    carta.classList.remove("flip-vertical-right", "carta");
+    carta.classList.remove("carta-vacia", "flip-vertical-right");
     carta.classList.add("flip-vertical-left");
-    carta.src = "/img/1.png"
+    carta.style.backgroundImage = "url('/img/1.png')";
 
     setTimeout(() => {
       carta.classList.remove("flip-vertical-left");
       carta.classList.add("flip-vertical-right", "carta-vacia");
+      carta.style.backgroundImage = "none";
     }, 1500);
   });
 }

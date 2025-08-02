de6b05ef1f5df06272c00699b33c64b9199453f6
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-08-02 19:58:31 +0200

    comenzando FINAL

--- FINAL/src/modelo/model.ts
+++ FINAL/src/modelo/model.ts
@@ -1,18 +1,22 @@
 export interface Carta {
-  idFoto: number; // id del 1 al 6 para 12 cartas, así identificamos rápido si es un gatito ,un perrito...
-  // el ID se repete 2 veces en el array de cartas (hay dos cartas de un perro, hay dos cartas de un gato)
-  imagen: string; // por comodidad repetimos la url de la imagen
+  idFoto: number;
+  imagen: string;
   estaVuelta: boolean;
   encontrada: boolean;
-}
+};
 
 interface InfoCarta {
   idFoto: number;
   imagen: string;
-}
+};
 
-const infoCartas: InfoCarta[] = [
-  /* Aquí ponemos seis cartas siguiendo la interfaz de InfoCarta */
+const infoCartas: InfoCarta[] = [  
+  { idFoto: 1, imagen: "/img/1.png"},
+  { idFoto: 2, imagen: "/img/2.png"},
+  { idFoto: 3, imagen: "/img/3.png"},
+  { idFoto: 4, imagen: "/img/4.png"},
+  { idFoto: 5, imagen: "/img/5.png"},
+  { idFoto: 6, imagen: "/img/6.png"},
 ];
 
 const crearCartaInicial = (idFoto: number, imagen: string): Carta => ({
@@ -23,17 +27,21 @@ const crearCartaInicial = (idFoto: number, imagen: string): Carta => ({
 });
 
 const crearColeccionDeCartasInicial = (infoCartas: InfoCarta[]): Carta[] => {
-  /* Aquí crearemos un array de cartas a partir de un array de infoCartas
-     y duplicaremos las cartas para que haya dos de cada tipo.
-  */
+  infoCartas.forEach((info) => {
+    const carta: Carta = {
+      idFoto: info.idFoto,
+      imagen: info.imagen,
+      estaVuelta: false,
+      encontrada: false,
+    };
+
+    cartas.push({ ...carta }, { ...carta });
+  });
+
+  return cartas;
 };
 
 export let cartas: Carta[] = crearColeccionDeCartasInicial(infoCartas);
-
-/*
-  Aquí definimos el tipo de estado de la partida, la idea es que cuando empiece la partida todas las cartas estén boca abajo y si se hacen click sobre ellas no se volteen.
-  EstadoPartida = "PartidaNoIniciada", una vez que se pulse Iniciar partida el estado de la partida cambiaría a "CeroCartasLevantadas" y así sucesivamente.
-*/
 
 type EstadoPartida =
   | "PartidaNoIniciada"
@@ -47,7 +55,7 @@ export interface Tablero {
   estadoPartida: EstadoPartida;
   indiceCartaVolteadaA?: number;
   indiceCartaVolteadaB?: number;
-}
+};
 
 const crearTableroInicial = (): Tablero => ({
   cartas: cartas,
--- FINAL/src/motor/motor.ts
+++ FINAL/src/motor/motor.ts
@@ -1,19 +1,31 @@
-/*
-En el motor nos va a hacer falta un método para barajar cartas
-*/
+import { Carta, Tablero, tablero } from "../modelo/model";
+
+// Barajamos las cartas
 const barajarCartas = (cartas : Carta[]): Carta[] => {
-  //...
+  const durstenfeldShuffle = <T>(array: T[]): T[] => {
+    const copia = [...array];
+    for (let i = copia.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copia[i], copia[j]] = [copia[j], copia[i]];
+    }
+    return copia;
+  };
+  return durstenfeldShuffle(cartas);
 }
 
-/*
-  Una carta se puede voltear si no está encontrada y no está ya volteada, o no hay dos cartas ya volteadas
-*/
+//Comprueba si una carta se puede voltear si no está encontrada y no está ya volteada, o no hay dos cartas ya volteadas
 const sePuedeVoltearLaCarta = (tablero: Tablero, indice: number ): boolean => {
-  //..
+  if (tablero.cartas[indice].encontrada || tablero.cartas[indice].estaVuelta || tablero.estadoPartida === "DosCartasLevantadas") {
+    return false;
+  }
+  return true;
 }
 
 const voltearLaCarta = (tablero: Tablero, indice: number) : void => {
-  //...
+  if (sePuedeVoltearLaCarta(tablero, indice)) {
+    tablero.cartas[indice].estaVuelta = true;
+    tablero.estadoPartida = "UnaCartaLevantada";
+  }
 }
 
 /*

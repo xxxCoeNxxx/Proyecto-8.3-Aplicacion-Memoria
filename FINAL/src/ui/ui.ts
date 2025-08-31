7d5561b3226af2d8fbc7f710ad8e41a5ddebb896
xxxCoeNxxx <pedrocela1992@gmail.com>
2025-08-31 16:10:28 +0200

    intentos funcionando

--- FINAL/src/ui/ui.ts
+++ FINAL/src/ui/ui.ts
@@ -4,6 +4,9 @@ import { iniciaPartida, ocultarCarta, sonPareja, voltearCarta, esPartidaCompleta
 let indicePrimeraCarta: number | null = null;
 let indiceSegundaCarta: number | null = null;
 let bloqueoClick = false;
+let intentos: number = 0;
+
+const intentosDiv = document.getElementById("intentos") as HTMLDivElement;
 
 // Botón de inicio
 export const crearBotonIniciar = () => {
@@ -16,14 +19,22 @@ export const crearBotonIniciar = () => {
       indicePrimeraCarta = null;
       indiceSegundaCarta = null;
       bloqueoClick = false;
+      intentos = 0;
+      intentosDiv.innerText = `Intentos: ${intentos}`;
 
       for (let nCarta = 1; nCarta <= 12; nCarta++) {
         const cartaDOM = document.getElementById(nCarta.toString()) as HTMLDivElement;
-        ocultarCarta(cartaDOM); 
+        cartaDOM.classList.remove("flip-vertical-left", "flip-vertical-right", "carta-vacia");
+        cartaDOM.classList.add("flip-vertical-left");
+        setTimeout(() => {
+          cartaDOM.classList.remove("flip-vertical-left");
+          cartaDOM.classList.add("flip-vertical-right", "carta-vacia");
+          cartaDOM.style.backgroundImage = "none";
+        }, 300);
       }
     });
   }
-};
+}; 
 
 console.log(tablero.estadoPartida);
 
@@ -33,47 +44,51 @@ for (let nCarta = 1; nCarta <= 12; nCarta++) {
   const cartaDOM = document.getElementById(nCarta.toString()) as HTMLDivElement;
 
   cartaDOM.addEventListener("click", () => {
-  if (bloqueoClick) return;
-  const indice = nCarta -1;
-  const carta = tablero.cartas[indice];
-  
-  if (carta.encontrada || carta.estaVuelta) return;
-
-  voltearCarta(cartaDOM, carta.imagen);
-  carta.estaVuelta = true;
-
-  if (tablero.estadoPartida === "UnaCartaLevantada") {tablero.estadoPartida = "DosCartasLevantadas"}
-  if (tablero.estadoPartida === "CeroCartasLevantadas") {tablero.estadoPartida = "UnaCartaLevantada"}
-  console.log(tablero.estadoPartida);
-
-  if (indicePrimeraCarta === null) {
-    indicePrimeraCarta = indice;
-  } else if (indiceSegundaCarta === null) {
-    indiceSegundaCarta = indice;
+    if (bloqueoClick) return;
+    const indice = nCarta -1;
+    const carta = tablero.cartas[indice];
     
-    const carta1DOM = document.getElementById((indicePrimeraCarta + 1).toString()) as HTMLDivElement;
-    const carta2DOM = document.getElementById((indiceSegundaCarta + 1).toString()) as HTMLDivElement;
+    if (carta.encontrada || carta.estaVuelta) return;
 
-    if (sonPareja(indicePrimeraCarta, indiceSegundaCarta, tablero)) {
-      indicePrimeraCarta = null;
-      indiceSegundaCarta = null;
+    voltearCarta(cartaDOM, carta.imagen);
+    carta.estaVuelta = true;
 
-      if (esPartidaCompleta(tablero)) {
-        tablero.estadoPartida = "PartidaCompleta";
-        console.log(tablero.estadoPartida);
-      }
-    } else {
-      bloqueoClick = true;
-      setTimeout(() => {
-        ocultarCarta(carta1DOM);
-        ocultarCarta(carta2DOM);
-        tablero.cartas[indicePrimeraCarta!].estaVuelta = false;
-        tablero.cartas[indiceSegundaCarta!].estaVuelta = false;
+    if (tablero.estadoPartida === "UnaCartaLevantada") {
+      tablero.estadoPartida = "DosCartasLevantadas"
+      intentos++;
+      intentosDiv.innerText = `Intentos: ${intentos}`;
+    }
+    if (tablero.estadoPartida === "CeroCartasLevantadas") {tablero.estadoPartida = "UnaCartaLevantada"}
+    console.log(tablero.estadoPartida);
+
+    if (indicePrimeraCarta === null) {
+      indicePrimeraCarta = indice;
+    } else if (indiceSegundaCarta === null) {
+      indiceSegundaCarta = indice;
+      
+      const carta1DOM = document.getElementById((indicePrimeraCarta + 1).toString()) as HTMLDivElement;
+      const carta2DOM = document.getElementById((indiceSegundaCarta + 1).toString()) as HTMLDivElement;
+
+      if (sonPareja(indicePrimeraCarta, indiceSegundaCarta, tablero)) {
         indicePrimeraCarta = null;
         indiceSegundaCarta = null;
-        bloqueoClick = false;
-      }, 1000);
-      }
-   }
+
+        if (esPartidaCompleta(tablero)) {
+          tablero.estadoPartida = "PartidaCompleta";
+          console.log(tablero.estadoPartida);
+        }
+      } else {
+        bloqueoClick = true;
+        setTimeout(() => {
+          ocultarCarta(carta1DOM);
+          ocultarCarta(carta2DOM);
+          tablero.cartas[indicePrimeraCarta!].estaVuelta = false;
+          tablero.cartas[indiceSegundaCarta!].estaVuelta = false;
+          indicePrimeraCarta = null;
+          indiceSegundaCarta = null;
+          bloqueoClick = false;
+        }, 1000);
+        }
+    }
   });
 }
